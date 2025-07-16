b38f4c2a987144367df4fef078bb8d66cd0c553c
gilert <crankyfox@protonmail.com>
2025-07-16 21:58:29 +0200

    add twa-dev/sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Map from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import WebApp from "@twa-dev/sdk";
 
 const App = () => {
+   const telegramEnvironment = WebApp.initData.length > 0;
+   const data = WebApp.initData;
   return (
     <div>
       <Map
@@ -14,7 +17,9 @@ const App = () => {
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="https://tiles.openfreemap.org/styles/liberty"
       />
-      <p className="text-4xl font-bold text-green-600 absolute top-0 dev-b-blue">TelegramWebApp Init object: </p>
+      <p className="text-4xl font-bold text-green-600 absolute top-0 dev-b-blue">TelegramWebApp Init object: 
+        { telegramEnvironment ? (<span>{{data}}</span>) : (<></>)}
+      </p>
     </div>
   );
 };
