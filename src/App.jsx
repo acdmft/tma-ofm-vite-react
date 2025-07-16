ae023a09b56316bbba4e80078b1376e3691d32e2
gilert <crankyfox@protonmail.com>
2025-07-16 21:58:29 +0200

    add twa-dev/sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Map from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import WebApp from "@twa-dev/sdk";
 
 const App = () => {
+  //  const tg = window.Telegram.WebApp;
+
+  // User data
+  // const user = tg.initDataUnsafe?.user;
+  const user = WebApp.initDataUnsafe.user;
+  if (user) {
+    console.log("User ID:", user.id);
+    console.log("Username:", user.username);
+    console.log("First Name:", user.first_name);
+    console.log("Last Name:", user.last_name);
+    console.log("Language Code:", user.language_code);
+  } else {
+    console.log("No user data available.");
+  }
+
   return (
     <div>
       <Map
@@ -14,7 +30,10 @@ const App = () => {
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="https://tiles.openfreemap.org/styles/liberty"
       />
-      <p className="text-4xl font-bold text-green-600 absolute top-0 dev-b-blue">TelegramWebApp Init object: </p>
+      <p className="text-4xl font-bold text-green-600 absolute top-0 dev-b-blue">
+        TelegramWebApp Init object:
+        {/* { telegramEnvironment ? (<span>{{data.user}}</span>) : (<></>)} */}
+      </p>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwinddcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwinddcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwinddcss()],
-})
+  // server: {
+  //   allowedHosts: [/\.ngrok-free\.app$/],
+  //   host: true,
+  // },
+});
