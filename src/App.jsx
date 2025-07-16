fe4f281fb4e36336e5a8195a907e77d6bb753569
gilert <crankyfox@protonmail.com>
2025-07-16 21:50:01 +0200

    add tailwindcss v.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import Map from 'react-map-gl/maplibre';
+import Map from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 const App = () => {
   return (
-    <Map 
-    initialViewState={{
-      longitude: -122.4,
-      latitude: 37.8,
-      zoom: 14
-    }}
-    style={{width: "100vw", height: "100vh"}}
-    mapStyle="https://tiles.openfreemap.org/styles/liberty"
-    />
-  )
+    <div>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="https://tiles.openfreemap.org/styles/liberty"
+      />
+      <p className="text-4xl font-bold text-green-600 absolute top-0 dev-b-blue">TelegramWebApp Init object: </p>
+    </div>
+  );
 };
 
 export default App;
